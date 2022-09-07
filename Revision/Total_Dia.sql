4f547c8350dabb44797db1f410aee41fe1880c78
Victor Manuel Guayara Suarez <61199510+guayara504@users.noreply.github.com>
2022-09-07 11:20:57 -0500

    Update Total_Dia.sql

--- Revision/Total_Dia.sql
+++ Revision/Total_Dia.sql
@@ -1,1 +1,5 @@
-SELECT * FROM z04_estado  WHERE digitador LIKE "%GUAYARA%" AND fecha_notificacion = CURDATE()
+SELECT digitador AS Digitador,COUNT(digitador) AS Total_Digitador,
+(SELECT count(digitador) FROM z04_estado WHERE fecha_notificacion = CURDATE() ) AS Total_Dia
+FROM z04_estado  WHERE digitador LIKE "%%" AND fecha_notificacion = CURDATE()
+GROUP BY digitador
+ORDER BY Total_Digitador DESC
